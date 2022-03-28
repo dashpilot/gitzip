7c8ee5004e9b9f12d5a7793ed414bf245b983a1f
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 01:53:52 +0200

    Update download.js

--- api/download.js
+++ api/download.js
@@ -6,12 +6,12 @@ var content = "inner content of the file";
 zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
 // add local file
 // zip.addLocalFile("./hello-world.txt");
-// zip.addLocalFile("index.html");
+zip.addLocalFile("./dist/index.html");
 
 var buffer = zip.toBuffer();
 
 console.log(buffer);
   
-response.status(200).download(buffer);
+// response.status(200).download(buffer);
   
 }
