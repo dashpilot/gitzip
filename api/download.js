cc3bebc33fb4f9008c76e8568ec31e6cebc9929f
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 01:32:09 +0200

    Update and rename index.js to api/download.js

--- api/download.js
+++ api/download.js
@@ -1,6 +1,7 @@
 var AdmZip = require("adm-zip");
 var zip = new AdmZip();
 
+export default async function handler(request, response) {
 var content = "inner content of the file";
 zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
 // add local file
@@ -10,3 +11,7 @@ zip.addLocalFile("./src/index.html");
 var buffer = zip.toBuffer();
 
 console.log(buffer);
+  
+response.status(200).download(buffer);
+  
+}
