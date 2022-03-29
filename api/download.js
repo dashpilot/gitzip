0514eab7125de393b5e67c0225e78b9592bca5f3
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 02:09:40 +0200

    This is working now

--- api/download.js
+++ api/download.js
@@ -2,22 +2,19 @@ var AdmZip = require("adm-zip");
 var zip = new AdmZip();
 
 export default async function handler(request, response) {
-var content = "inner content of the file";
-zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
-// add local file
-// zip.addLocalFile("./hello-world.txt");
-zip.addLocalFile("./dist/index.html");
-
-var buffer = zip.toBuffer();
-
-console.log(buffer);
   
-// response.status(200).download(buffer);
+  var content = "inner content of the file";
+  zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
+  
+  // add local file
+  zip.addLocalFile("./hello-world.txt");
+  zip.addLocalFile("./dist/index.html");
+
+  var buffer = zip.toBuffer();
   
   response.setHeader("Content-Type", "application/octet-stream");
-  response.setHeader("Content-Disposition", "inline;filename=somefile.zip");
+  response.setHeader("Content-Disposition", "inline;filename=myfile.zip");
 
-  
   response.end(buffer);
   
 }
