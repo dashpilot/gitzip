769d11704b483b5063e5020f4cfec381faaae84a
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 13:11:05 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
-fs.writeFileSync("./api/zip/dynamic.txt", "This file is the result of a build step");
+fs.writeFileSync("./dist/dynamic.txt", "This file is the result of a build step");
 console.log("build step completed");
