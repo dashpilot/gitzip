041a070683e0e14c7e454dcd8ca1412dc4bf218c
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 13:00:25 +0200

    Let's try without hardcoding

--- api/download.js
+++ api/download.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
+const { join } = require('path')
 var AdmZip = require("adm-zip");
 var zip = new AdmZip();
 
 export default async function handler(request, response) {
   
   var content = "inner content of the file";
-  zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
+  zip.addFile("at-runtime.txt", Buffer.from(content, "utf8"), "entry comment goes here");
   
   
-  var filenames = fs.readdirSync(".output/");
+  var filenames = fs.readdirSync(join(__dirname, 'zip'));
   filenames.forEach(file => {
     console.log(file);
-    zip.addLocalFile(".output/"+file);
+    zip.addLocalFile(join(__dirname, 'zip')+file);
   });
  
   
