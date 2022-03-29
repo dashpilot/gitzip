4c936f4945a58b5cbba86584daff514059f3caf7
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-29 02:14:04 +0200

    Update download.js

--- api/download.js
+++ api/download.js
@@ -7,7 +7,7 @@ export default async function handler(request, response) {
   zip.addFile("test.txt", Buffer.from(content, "utf8"), "entry comment goes here");
   
   // add local file
-  zip.addLocalFile("./hello-world.txt");
+  zip.addLocalFile("./dist/hello-world.txt");
   zip.addLocalFile("./dist/index.html");
 
   var buffer = zip.toBuffer();
